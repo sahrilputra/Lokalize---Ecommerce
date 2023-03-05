2278e1e74f23dc340f6e7e301d4709598c40de5f
sahrilputra <iam.sahrilputra@gmail.com>
2023-03-05 15:40:59 +0700

    fixing image

--- components/market/banner.js
+++ components/market/banner.js
@@ -5,17 +5,20 @@ import { Carousel } from 'flowbite-react';
 export const Banner = () => {
     return (
         <>
-        <div className={styles.banner}>
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-                <Carousel slideInterval={5000}>
-                    <img src="/banner/banner_ads.png" alt="banner" />
-                    <img src="/banner/banner_ads.png" alt="banner" />
-                </Carousel>
-            </div>
             <div className={styles.banner}>
-                <img src="/banner/banner_ads.png" alt="banner" />
+                <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+                    <Carousel slideInterval={5000}>
+                        <Image
+                            src="/banner/banner_ads.png"
+                            alt="banner"
+                        />
+                        <Image
+                            src="/banner/banner_ads.png"
+                            alt="banner"
+                        />
+                    </Carousel>
+                </div>
             </div>
-        </div>
         </>
     )
 }
