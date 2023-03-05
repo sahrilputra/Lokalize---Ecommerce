0a813e9c1a5a6c63eace7bffffb4a65d9983f976
sahrilputra <iam.sahrilputra@gmail.com>
2023-03-06 01:30:45 +0700

    added user menu's

--- components/header/UserMenu.js
+++ components/header/UserMenu.js
@@ -6,7 +6,30 @@ import { signOut, signIn } from 'next-auth/react'
 export const UserMenu = ({session}) => {
     return (
         <>
-        
+        <div className={styles.menu}>
+            <div className={styles.menu__contaienr}>
+                <div className={styles.menu__profile}>
+
+                </div>
+                <h1>Hello Bestie,</h1>
+            </div>
+            <ul>
+                <li>
+                    <Link href="#">Account</Link>
+                </li>
+                <li>
+                    <Link href="#">Order</Link>
+                </li>
+                <li>
+                    <Link href="#">Setting</Link>
+                </li>
+            </ul>
+
+            <div className={styles.flex}>
+                <button className={styles.btn__primary}></button>
+                <button className={styles.btn__secondary}></button>
+            </div>
+        </div>
         </>
     )
 }
