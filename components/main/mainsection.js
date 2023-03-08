260fb94b5bd797cf827346ec85953f1f39cd4e0d
sahrilputra <iam.sahrilputra@gmail.com>
2023-03-08 13:18:10 +0700

    adding review in main content

--- components/main/mainsection.js
+++ components/main/mainsection.js
@@ -4,12 +4,14 @@ import { HeroSection } from './HeroSection'
 import { DisplayProduct } from './displayProduct'
 import { Content } from './content'
 import { Subscribe } from './subscribe';
+import { ReviewComponent } from './review';
 export const Mainsection = () => {
   return (
     <>
     <HeroSection />
     <DisplayProduct/>
     <Content />
+    <ReviewComponent />
     </>
 
   )
