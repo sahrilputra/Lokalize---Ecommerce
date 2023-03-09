6cf197fe49c97889915ae4ba2d48943f381318d7
sahrilputra <iam.sahrilputra@gmail.com>
2023-03-10 05:51:49 +0700

    adding search component and fixing quick view:

--- components/market/card.js
+++ components/market/card.js
@@ -1,6 +1,7 @@
 import styles from '../market/styles.module.scss';
 import Image from 'next/image';
-import { HiOutlineHeart, HiOutlineShoppingCart } from "react-icons/hi";
+import { HiOutlineHeart, HiOutlineShoppingCart } from "react-icons/hi"
+import { FaSearch } from 'react-icons/fa';
 import data from '../../models/data'
 import { Modal, Card } from 'flowbite-react';
 import { QuickView } from './quickView';
@@ -30,6 +31,7 @@ export const CardComponent = () => {
   const handleClose = () => {
     setSelectedItem(null);
   };
+
   // ============
 
   const [noFil, setNone] = useState();
@@ -38,6 +40,8 @@ export const CardComponent = () => {
   const [showing, setShowing] = useState(false);
   const [favorite, setFavorite] = useState([]);
   const [menuHidden, setMenuHidden] = useState(false);
+
+
   const handlerFavorite = (item) => {
     setFavorite([...favorite, item]);
   }
@@ -59,6 +63,16 @@ export const CardComponent = () => {
 
       <div className={styles.market}>
         <h3 className={styles.market__tag}>Discover</h3>
+        <div className="relative flex-1">
+          <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+            <FaSearch className="text-gray-500" />
+          </span>
+          <input
+            className="block sm:w-full md:w-64 pl-10 pr-4 py-2 border border-gray-300 rounded-md leading-5 bg-white text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 transition duration-150 ease-in-out"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
         <div className={styles.market__container}>
           <div className={styles.market__menuSection}>
             <div className={styles.market__item} onClick={() => handlerFilter('all')} >
