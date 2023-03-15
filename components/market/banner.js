e9e148b4bae8dd76b5812b2274778389cf4d34df
Sahrilputra <iam.sahrilputra@gmail.com>
2023-03-15 01:53:48 +0000

    Update 3 files

--- components/market/banner.js
+++ components/market/banner.js
@@ -1,28 +1,25 @@
-
-import styles from '../market/styles.module.scss';
-import Image from 'next/image';
-import { Carousel } from 'flowbite-react';
+import styles from "../market/styles.module.scss";
+import Image from "next/image";
+import { Carousel } from "flowbite-react";
 export const Banner = () => {
-    return (
-        <>
-            <div className={styles.banner}>
-                <div className="mt-28 h-32 sm:h-64 xl:h-80 2xl:h-96">
-                    <Carousel slideInterval={5000}>
-                        <Image
-                            src="/banner/banner_ads.png"
-                            alt="banner"
-                            width={220}
-                            height={250}
-                        />
-                        <Image
-                            src="/banner/banner_ads.png"
-                            alt="banner"
-                            width={320}
-                            height={250}
-                        />
-                    </Carousel>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={styles.banner}>
+        <div className="mt-28 h-32 sm:h-64 xl:h-80 2xl:h-96">
+          <Carousel slideInterval={5000}>
+            <img
+              src="../../banner/banner_ads.png"
+              alt="banner ads"
+              className={styles.ads}
+            />
+            <img
+              src="../../banner/banner_ads.png"
+              alt="banner ads"
+              className={styles.ads}
+            />
+          </Carousel>
+        </div>
+      </div>
+    </>
+  );
+};
